fa6a4dbc471b2b98beae930cb44a19adf931f491
JyotindraTavanoji <jktbusiness21@gmail.com>
2021-08-16 20:22:18 +0530

    finale

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -49,6 +49,7 @@ function Orders({ orders }) {
 export default Orders;
 
 export async function getServerSideProps(context) {
+    const session = await getSession(context);
     const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
     //Get the users credentials
@@ -88,6 +89,7 @@ export async function getServerSideProps(context) {
     return {
         props: {
             orders,
+            session,
         },
     };
 }
